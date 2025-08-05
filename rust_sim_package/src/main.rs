3348045da6cb9f7d734c8caff170196d0eac9349
Mike <michaelrossking97@gmail.com>
2025-08-05 07:53:28 +0000

    changing Person to Agent, and starting resource interruptions

--- rust_sim_package/src/main.rs
+++ rust_sim_package/src/main.rs
@@ -1,7 +1,6 @@
 use std::collections::{HashMap, VecDeque, BinaryHeap};
 use std::cmp::Reverse;
 use std::fmt;
-use std::thread::spawn;
 
 
 
@@ -23,14 +22,14 @@ impl Default for Environment {
 
 impl Environment {
 
-    fn timeout(&mut self, person:&Person, process_time:u64) {
+    fn timeout(&mut self, agent:&Agent, process_time:u64) {
 
         self.event_list
-            .entry(person.id)
+            .entry(agent.id)
             .or_insert_with(VecDeque::new)
             .push_back(
                 IdleEvent {
-                    id:person.id,
+                    id:agent.id,
                     event_type:EventType::Timeout,
                     process_time:process_time,
                     target:String::new(),
@@ -38,14 +37,14 @@ impl Environment {
             );
     }
 
-    fn enter_queue(&mut self, person:&Person, process_time:u64, target:String) {
+    fn enter_queue(&mut self, agent:&Agent, process_time:u64, target:String) {
 
         self.event_list
-            .entry(person.id)
+            .entry(agent.id)
             .or_insert_with(VecDeque::new)
             .push_back(
                 IdleEvent {
-                    id:person.id,
+                    id:agent.id,
                     event_type:EventType::EnterQueue,
                     process_time:process_time,
                     target:target
@@ -70,14 +69,16 @@ impl Environment {
 
     }
 
-    fn resource_interruption(
-        &mut self,
-        id:u64,
-        spawn_time:u64,
-        target:String,
-        capacity_interrupted:u64,
-        duration:u64,
-    ) {
+    fn resource_interruption(&mut self, interruption:ResourceInterruption) {
+
+        if let Some(resource) = self.resources.get_mut(&interruption.target) {
+            resource.interruption_queue.push((Reverse(interruption.spawn_time), interruption));
+        } else {
+            println!(
+                "Failed adding interruption id {}. Resource target {} does not exist.",
+                interruption.id, interruption.target
+            );
+        }
 
     }
 
@@ -130,7 +131,7 @@ impl Environment {
                         EventType::Timeout => {
 
                             sim_logs.push(
-                                format!("{}: Person id {} is executing event {}.", sim_time, event.id, event.event_type)
+                                format!("{}: Agent id {} is executing event {}.", sim_time, event.id, event.event_type)
                             );
 
                             // sim time + timeout time.
@@ -149,7 +150,7 @@ impl Environment {
                         EventType::EndTimeout => {
 
                             sim_logs.push(
-                                format!("{}: Person id {} is executing event {}.", sim_time, event.id, event.event_type)
+                                format!("{}: Agent id {} is executing event {}.", sim_time, event.id, event.event_type)
                             );
 
                             // Pop the next event for this id, and schedule it to be current.
@@ -171,17 +172,13 @@ impl Environment {
                         EventType::EnterQueue => {
 
                             sim_logs.push(
-                                format!("{}: Person id {} is being added to {}.", sim_time, event.id, event.target)
+                                format!("{}: Agent id {} is being added to {}.", sim_time, event.id, event.target)
                             );
 
                             self.resource_queues
                                 .get_mut(&event.target)
                                 .expect("User defined queue name.")
                                 .push_back((event.id, event.process_time));
-
-                        }
-
-                        EventType::ResourceInterruption => {
 
                         }
 
@@ -203,7 +200,7 @@ impl Environment {
 
                     sim_logs.push(
                         format!(
-                            "{}: {}, id {}, released Person id {}.",
+                            "{}: {}, id {}, released Agent id {}.",
                             sim_time, resource_name, resource_capacity_id, resource_capacity.current_agent_id
                         )
                     );
@@ -213,7 +210,7 @@ impl Environment {
 
                             sim_logs.push(
                                 format!(
-                                    "{}: Person id {} has event type {} moved from events list to scheduled events.",
+                                    "{}: Agent id {} has event type {} moved from events list to scheduled events.",
                                     sim_time, resource_capacity.current_agent_id, idle_event.event_type
                                 )
                             );
@@ -245,7 +242,7 @@ impl Environment {
                         if let Some((agent_id, agent_sim_time)) = queue.pop_front() {
                             sim_logs.push(
                                 format!(
-                                    "{}: Adding Person id {} from {} to {} id {}.",
+                                    "{}: Adding Agent id {} from {} to {} id {}.",
                                     sim_time, agent_id, resource.queue_target, resource.resource_name, resource_capacity_id
                                 )
                             );
@@ -297,7 +294,7 @@ impl Environment {
                 } else {
                     sim_time = min_resource_available_time;
                 }
-            }
+            }            
 
             loops -= 1;
         }
@@ -313,7 +310,6 @@ enum EventType {
     Timeout,
     EndTimeout,
     EnterQueue,
-    ResourceInterruption,
 }
 
 impl fmt::Display for EventType {
@@ -322,7 +318,6 @@ impl fmt::Display for EventType {
             EventType::Timeout => "timeout",
             EventType::EndTimeout => "end_timeout",
             EventType::EnterQueue => "enter_queue",
-            EventType::ResourceInterruption => "resource_interruption",
         };
         write!(f, "{}", s)
     }
@@ -333,6 +328,7 @@ struct Resource {
     resource_name:String,
     queue_target:String,
     status:HashMap<u64, ResourceStatus>,
+    interruption_queue:BinaryHeap<(Reverse<u64>, ResourceInterruption)>,
 }
 
 impl Resource {
@@ -342,7 +338,8 @@ impl Resource {
         let mut new_resource:Resource = Resource {
             resource_name:resource_name,
             queue_target:queue_target,
-            status:HashMap::new()
+            status:HashMap::new(),
+            interruption_queue:BinaryHeap::new(),
         };
 
         for cap in 1..capacity+1 {
@@ -379,7 +376,7 @@ struct IdleEvent {
     target:String,
 }
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 struct ActiveEvent {
     id:u64,
     event_type:EventType,
@@ -402,7 +399,7 @@ impl ActiveEvent {
 
 impl Ord for ActiveEvent {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.id.cmp(&other.id) // or any stable fallback comparison
+        self.id.cmp(&other.id)
     }
 }
 
@@ -413,7 +410,7 @@ impl PartialOrd for ActiveEvent {
 }
 
 #[derive(Debug)]
-struct Person {
+struct Agent {
     id:u64,
     spawn_time:u64,
     process_time:u64,
@@ -421,7 +418,7 @@ struct Person {
 
 
 
-impl Person {
+impl Agent {
 
     fn process(self, sim_env:&mut Environment) {
         sim_env.timeout(&self, self.spawn_time);
@@ -430,53 +427,51 @@ impl Person {
 
 }
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct ResourceInterruption {
     id:u64,
+    spawn_time:u64,
+    target:String,
+    capacity_interrupted:u64,
+    duration:u64,
 }
 
-impl ResourceInterruption {
-
-    fn add_interruption(
-        &self,
-        spawn_time:u64,
-        target:String,
-        capacity_interrupted:u64,
-        duration:u64,
-        sim_env:&mut Environment,
-    ) {
-        sim_env.resource_interruption(
-            self.id,
-            spawn_time,
-            target,
-            capacity_interrupted,
-            duration,
-        );
-    }
-
-}
 
 
 fn main() {
 
-    let people:Vec<Person> = vec![
-        Person {id:1,spawn_time:3,process_time:1},
-        Person {id:2,spawn_time:3,process_time:7},
-        Person {id:3,spawn_time:3,process_time:10},
+    let agents:Vec<Agent> = vec![
+        Agent {id:1,spawn_time:3,process_time:1},
+        Agent {id:2,spawn_time:3,process_time:7},
+        Agent {id:3,spawn_time:3,process_time:10},
     ];
 
     let mut sim_env:Environment = Environment::default();
     let resource_queue:SimulationQueue = SimulationQueue { queue_name: "queue_1".to_string(), queue: VecDeque::new() };
     let resource_a:Resource = Resource::new_resource(2, "resource_1".to_string(), "queue_1".to_string());
     let resource_b:Resource = Resource::new_resource(1, "resource_2".to_string(), "queue_1".to_string());
+
+    let interruptions:Vec<ResourceInterruption> = vec![
+        ResourceInterruption {
+            id:1,
+            spawn_time:1,
+            target:"resource_1".to_string(),
+            capacity_interrupted:2,
+            duration:2,
+        }
+    ];
     
-    for person in people.into_iter() {
-        person.process(&mut sim_env);
+    for agent in agents.into_iter() {
+        agent.process(&mut sim_env);
     }
 
     sim_env.add_resource(resource_a);
     sim_env.add_resource(resource_b);
     sim_env.add_queue(resource_queue);
 
+    for interruption in interruptions.into_iter() {
+        sim_env.resource_interruption(interruption);
+    }
 
     let sim_logs:Vec<String> = sim_env.run_sim();
 
